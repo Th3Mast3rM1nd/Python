3f31b8d6744fbe9acdaf090dfa8c555abfcff860
MasterMind <th3mast3rm1nd@MasterMinds-MacBook-Air.local>
2021-11-03 14:51:30 +0100

    Python Day1 Basics

---
+++ Day1/name_cases.py
@@ -0,0 +1,19 @@
+name="mastermind"
+print(f"Hello {name}")
+print(f"{name.upper()}")
+print(f"{name.lower()}")
+name_2 = "the"
+full_name = f"{name_2} {name}"
+print(f"{full_name.title()}")
+
+author_name = "Albert Einstein"
+print(f"\t{author_name} once Said, \" A Person who never made a\n\tmistake never tried anything new" )
+
+name_with_spaces = "    Mast3rM1nd"
+name_with_spaces_1 = "Th3Mat3rM1nd        "
+print(f"{name_with_spaces} {name_with_spaces_1}")
+name_without_spaces = name_with_spaces.lstrip()
+name_without_spaces_1 = name_with_spaces_1.rstrip()
+print(f"{name_without_spaces} {name_without_spaces_1}")
+
+
