7ae57efdc75a0c60c84e7e95836b4a32626a4a93
MasterMind <th3mast3rm1nd@MasterMinds-MacBook-Air.local>
2021-11-03 15:02:14 +0100

    Day1 Varibales and Strings

--- Day1/Day01_Script.py
+++ Day1/Day01_Script.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+
+#
 name="mastermind"
 print(f"Hello {name}")
 print(f"{name.upper()}")
 print(f"{name.lower()}")
+
 name_2 = "the"
 full_name = f"{name_2} {name}"
 print(f"{full_name.title()}")
@@ -13,8 +16,11 @@ print(f"\t{author_name} once Said, \" A Person who never made a\n\tmistake never
 name_with_spaces = "    Mast3rM1nd"
 name_with_spaces_1 = "Th3Mat3rM1nd        "
 print(f"{name_with_spaces} {name_with_spaces_1}")
+
 name_without_spaces = name_with_spaces.lstrip()
 name_without_spaces_1 = name_with_spaces_1.rstrip()
 print(f"{name_without_spaces} {name_without_spaces_1}")
 
+number = 10_10_1000_00000
+print(number)
 
