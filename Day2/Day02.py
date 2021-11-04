95f0f9e41e7ea93e7ef4f8d0920957644801170f
MasterMind <th3mast3rm1nd@MasterMinds-MacBook-Air.local>
2021-11-04 18:39:06 +0100

    new files

--- Day2/Day02.py
+++ Day2/Day02.py
@@ -1,13 +1,15 @@
-print(f"Lists")
+print("Lists")
 
 ips = ["192.168.1.1","192.168.1.23","192.168.1.107"]
-
 print(f" there is {len(ips)} in the list ")
-
 # adding new ips to the list 
 
 ips.insert(0,"10.10.12.10")
 ips.append("192.168.11.110")
 ips.reverse()
 print(ips)
+router_ip = "192.168.1.1"
+ips.remove(router_ip)
+print(ips)
+
 
