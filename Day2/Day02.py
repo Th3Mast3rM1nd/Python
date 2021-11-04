2469149e27ccd95313a75b057eb862d94871396c
MasterMind <th3mast3rm1nd@MasterMinds-MacBook-Air.local>
2021-11-04 18:32:06 +0100

    Changing Files names

---
+++ Day2/Day02.py
@@ -0,0 +1,13 @@
+print(f"Lists")
+
+ips = ["192.168.1.1","192.168.1.23","192.168.1.107"]
+
+print(f" there is {len(ips)} in the list ")
+
+# adding new ips to the list 
+
+ips.insert(0,"10.10.12.10")
+ips.append("192.168.11.110")
+ips.reverse()
+print(ips)
+
