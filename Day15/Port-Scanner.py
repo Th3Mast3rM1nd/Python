0cb833f7a72f00aba3d930ac52f861928b66c81a
MasterMind <th3mast3rm1nd@MasterMinds-MacBook-Air.local>
2021-11-24 16:29:35 +0100

    Day 15

--- Day15/Port-Scanner.py
+++ Day15/Port-Scanner.py
@@ -2,19 +2,12 @@ import socket
 # only 1023 Well-Known Ports 
 ip = input("Enter IP Here ")
 def port_scanner(port):
-    try:
-        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-        s.settimeout(4)
-        s.connect((ip,int(port)))
-        s.settimeout(None)
-    except socket.timeout :
-        pass
-    else:
-        s.send(b"")
-        respond = s.recv(4096)
-        return respond
-
+    s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    open_port = s.connect_ex((ip,int(port)))
+    s.close()
+    return open_port
+        
 for i in range(1,1023):
-    if port_scanner(i):
+    if port_scanner(i) == 0 :
         print(f"port {i} is Open")
-        print(port_scanner(i))
