9957e10722b47f272ae4e1619cdb68e59c2866aa
MasterMind <th3mast3rm1nd@MasterMinds-MacBook-Air.local>
2021-11-24 13:08:08 +0100

    Day 15

---
+++ Day15/Day15.py
@@ -0,0 +1,19 @@
+import socket 
+# TCP client 
+target_host = "192.168.1.107"
+target_port = 22
+
+# AF_INET = ipv4 or hostname
+# SOCK_STREAM = TCP Protocol 
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connecting to server using ip and port 
+client.connect((target_host,target_port))
+# sending data to the target_host 
+client.send(b"")
+# Recive the data from the server 
+response = client.recv(4096)
+
+print(response)
+
+
